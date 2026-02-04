3cf68378a26ab9f8f1675b6e33042a6014de6aa9
YasinHossain <67205970+YasinHossain@users.noreply.github.com>
2026-02-04 09:17:21 +0600

    chore: remove deprecated expo-router babel plugin

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['nativewind/babel', 'babel-preset-expo'],
-    plugins: ['expo-router/babel', 'react-native-reanimated/plugin'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
