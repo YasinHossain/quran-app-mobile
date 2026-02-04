e11352c9f39f49e580fd0ec6636e9a58b5f7b835
YasinHossain <67205970+YasinHossain@users.noreply.github.com>
2026-02-04 09:16:46 +0600

    fix: apply NativeWind preset after Expo

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'nativewind/babel'],
+    presets: ['nativewind/babel', 'babel-preset-expo'],
     plugins: ['expo-router/babel', 'react-native-reanimated/plugin'],
   };
 };
